2df79fc8b587bb6f7042bd7d93fd4ab100f26daf
Raghuveer1809 <56012344+Raghuveer1809@users.noreply.github.com>
2020-12-19 00:36:06 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include<stdio.h>
 #include<omp.h>
 #include<stdlib.h>
@@ -6,63 +7,74 @@
 
 int main()
 {
-float          *Array, *Check, serial_sum, sum, partialsum;
-int             array_size, i;
+	float* Array, * Check, serial_sum, sum, partialsum;
+	int             array_size, i;
 
-printf("Enter the size of the array\n");
-scanf("%d", &array_size);
+	printf("Enter the size of the array\n");
+	scanf_s("%d", &array_size);
 
-if (array_size <= 0) {
-printf("Array Size Should Be Of Positive Value ");
-exit(1);
-}
-/* Dynamic Memory Allocation */
+	if (array_size <= 0) {
+		printf("Array Size Should Be Of Positive Value ");
+		exit(1);
+	}
+	/* Dynamic Memory Allocation */
 
-Array = (float *)malloc(sizeof(float) * array_size);
-Check = (float*)malloc(sizeof(float) * array_size);
+	Array = (float*)malloc(sizeof(float) * array_size);
+	Check = (float*)malloc(sizeof(float) * array_size);
 
-/* Array Elements Initialization */
+	/* Array Elements Initialization */
 
-for (i = 0; i < array_size; i++) {
-Array[i] = i * 5;
-Check[i] = Array[i];
-}
+	for (i = 0; i < array_size; i++) {
+		Array[i] = i * 5;
+		Check[i] = Array[i];
+	}
 
-printf("The Array Elements Are \n");
+	printf("The Array Elements Are \n");
 
-for (i = 0; i < array_size; i++)
-printf("Array[%d]=%f\n", i, Array[i]);
+	for (i = 0; i < array_size; i++)
+		printf("Array[%d]=%f\n", i, Array[i]);
 
-sum = 0.0;
-partialsum = 0.0;
+	sum = 0.0;
+	partialsum = 0.0;
 
-/* OpenMP Parallel For Directive And Critical Section */
+	/* OpenMP Parallel For Directive And Critical Section */
 
 #pragma omp parallel for shared(sum)
-for (i = 0; i < array_size; i++) {
+	for (i = 0; i < array_size; i++) {
 #pragma omp critical
-sum = sum + Array[i];
+		sum = sum + Array[i];
 
+	}
+
+	serial_sum = 0.0;
+
+	/* Serail Calculation */
+	for (i = 0; i < array_size; i++)
+		serial_sum = serial_sum + Check[i];
+
+
+	if (serial_sum == sum)
+		printf("\nThe Serial And Parallel Sums Are Equal\n");
+	else {
+		printf("\nThe Serial And Parallel Sums Are UnEqual\n");
+		exit(1);
+	}
+
+	/* Freeing Memory */
+	free(Check);
+	free(Array);
+
+	printf("\nThe SumOfElements Of The Array Using OpenMP Directives Is %f\n", sum);
+	printf("\nThe SumOfElements Of The Array By Serial Calculation Is %f\n", serial_sum);
 }
 
-serial_sum = 0.0;
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
 
-/* Serail Calculation */
-for (i = 0; i < array_size; i++)
-serial_sum = serial_sum + Check[i];
-
-
-if (serial_sum == sum)
-printf("\nThe Serial And Parallel Sums Are Equal\n");
-else {
-printf("\nThe Serial And Parallel Sums Are UnEqual\n");
-exit(1);
-}
-
-/* Freeing Memory */
-free(Check);
-free(Array);
-
-printf("\nThe SumOfElements Of The Array Using OpenMP Directives Is %f\n", sum);
-printf("\nThe SumOfElements Of The Array By Serial Calculation Is %f\n", serial_sum);
-}
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
